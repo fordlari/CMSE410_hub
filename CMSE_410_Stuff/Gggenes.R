caf7132da0f26b7df2201d27e76ef395f30fba8f
Larissa Ford <fordlari@msu.edu>
2021-04-04 15:43:06 -0400

    Attempt 2 to add CMSE_410_Stuff file. Trial, folder will be disolved at a later date

---
+++ CMSE_410_Stuff/Gggenes.R
@@ -0,0 +1,9 @@
+
+library(ggplot2)
+library(gggenes)
+ggplot(example_genes, aes(xmin = start, xmax = end, y = molecule, fill = gene)) +
+  geom_gene_arrow() +
+  facet_wrap(~ molecule, scales = "free", ncol = 1) +
+  scale_fill_brewer(palette = "Set3")
+
+
